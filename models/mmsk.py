5868b552cabd42a97e9cf56b310b629ca8cd86df
Dimitri <dimitri.s.amado@gmail.com>
2025-11-04 12:08:50 -0300

    refactor: Remove redundant comments from MMsK class methods

--- models/mmsk.py
+++ models/mmsk.py
@@ -29,15 +29,12 @@ class MMsK(BaseQueueModel):
         }
 
     def __calculate_probability_system_empty(self) -> float:
-        """ P0 = (1 - p) / (1 - p^(K + 1)) """
         return (1 - self.rho) / (1 - self.rho**(self.k + 1))
 
     def __calculate_avg_customers_system(self) -> float:
-        """ L = (p / (1 - p)) - ((K + 1) * p^(K + 1) / (1 - p^(K + 1))) """
         return (self.rho / (1 - self.rho) - ((self.k + 1) * self.rho**(self.k + 1) / (1 - self.rho**(self.k + 1))))
 
     def __calculate_avg_customers_queue(self, l: float, p0: float) -> float:
-        """ Lq = L - (1 - P0) """
         return l - (1 - p0)
 
     def __calculate_probability_n_customers_system(self, n: int) -> float:
@@ -48,9 +45,7 @@ class MMsK(BaseQueueModel):
         return pn
 
     def __calculate_avg_time_queue(self, lq: float, pn: float) -> float:
-        """ Wq = Lq / λ(1 - Pn) """
         return lq / (self.lamb * (1 - pn))
 
     def __calculate_avg_time_system(self, l: float, pn: float) -> float:
-        """ W = L / λ(1 - Pn) """
         return l / (self.lamb * (1 - pn))
